ccb360afae5fc757c0b94456bed5a8a438351f71
sachin shukla <sachinshukla232003@gmail.com>
2025-03-05 16:45:50 +0530

    [Sachin Shukla]Add. click on logo launch website of bridgeLab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,12 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'Helloworld';
   imageUrl = "../assets/bridgeLablogo.avif";
+  url="https://www.bridgelabz.com/";
   ngOnInit(): void{
-    this.title= "hello from brideLabz";
+    this.title= "Hello From BridgeLabz";
+  }
+  onClick(event:Event){
+  console.log("Button was clickede",event)
+  window.open(this.url,"_blank");
   }
 }
